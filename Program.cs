b1cf2f3be6ae6e9120a68d3606a0f423b9bf1f23
Aarni Koskela <akx@iki.fi>
2018-06-15 17:32:34 +0300

    POC

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace cfg
+{
+    class Program
+    {
+        static Dictionary<string, List<string[]>> ParseGrammar(string filename) {
+            var rules = new Dictionary<string, List<string[]>>();
+            foreach (var line in File.ReadAllLines(filename))
+            {
+                if(line.Trim().Length == 0 || line.StartsWith("#")) continue;
+                var split = line.Split("->", 2, StringSplitOptions.RemoveEmptyEntries);
+                if(split.Length == 1) continue;
+                var rule = split[0].Trim();
+                var productions = split[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if(!rules.ContainsKey(rule)) {
+                    rules[rule] = new List<string[]>();
+                }
+                rules[rule].Add(productions);
+            }
+            return rules;
+        }
+        static void Main(string[] args)
+        {
+            var rules = ParseGrammar("grammar.txt");
+            foreach (var rule in rules)
+            {
+                Console.WriteLine(rule.Key + ":");
+                foreach (var productions in rule.Value)
+                {
+                    Console.WriteLine("  " + String.Join(' ', productions));
+                }
+            }
+            
+        }
+    }
+}
